8f16a53c0c838acd480dd5aec6085cb1ea76a65d
Giang2705 <95325796+Giang2705@users.noreply.github.com>
2024-06-11 02:53:19 +0700

    giang/BE login method

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,3 +9,25 @@ exports.register = async(req,res,next)=>{
         res.json({status: false, message: error.message})
     }
 }
+
+exports.login = async(req,res,next)=>{
+    try {
+        const {email, password} = req.body;
+        const user = await UserService.checkUser(email)
+        if (!user) {
+            throw new Error('User does not exist');
+        }
+        const isMatch = await user.comparePassword(password)
+        if (!isMatch) {
+            throw new Error('Wrong password');
+        }
+
+        let tokenData = {_id: user._id, email: user.email};
+
+        const token = await UserService.generateToken(tokenData, 'secretKet', '1h')
+        
+        res.status(200).json({status: true, token: token})
+    } catch (error) {
+        res.json({status: false, message: error.message})
+    }
+}
