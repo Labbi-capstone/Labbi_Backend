6b552d861ed5046749065c36325bacc7671655c0
minh0811 <vokhaiminh0811@gmail.com>
2024-06-12 19:40:28 +0700

    Reapply .gitignore

--- README.md
+++
@@ -1,1 +0,0 @@
-# Labbi-Backend
--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connection = mongoose.createConnection('mongodb+srv://hagiangnguyen2705:Giang270599@cluster0.u1tprem.mongodb.net/').on('open', ()=>{
-    console.log("Database connected")
-}).on('error', ()=>{
-    console.log("Connection error!")
-})
-
-module.exports = connection;
