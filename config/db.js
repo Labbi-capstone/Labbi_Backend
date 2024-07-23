eb393492f07f9c1246e4b497e4ad5f73bb5f4bd7
Võ Khải Minh <vokhaiminh0811@gmail.com>
2024-07-23 17:06:43 +0700

    Fixed connection to mongo Database, Bugs: User validation failed: name: Path ame is required.

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+console.log("Starting test-db.js script");
+console.log(`MONGO_URI: ${process.env.MONGO_URI}`);
 
 const connectDB = async () => {
+  console.log("Connecting to MongoDB"); // Debugging line
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
       serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
     });
 
