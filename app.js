86a2c5e454d488c61f8456b6fec2cebff972ab99
Võ Khải Minh <vokhaiminh0811@gmail.com>
2024-08-17 03:45:26 +0700

    add cors for localhost 3000

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // Import necessary modules for the application
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import userRouter from "./routes/userRoute.js";
 import organizationRouter from "./routes/organizationRoute.js";
 
 // Initialize express application
 const app = express();
+app.use(cors()); // This enables CORS for all origins
+
+// More specific setup:
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Adjust the origin if needed
+  })
+);
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
